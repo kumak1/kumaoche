88da055bf3d373a03da4aaa228e51f69fa122cbb
kumak1 <makino@pepabo.com>
2019-11-20 01:02:32 +0900

    update setup

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ setup(
     long_description='',
     author='kumak1',
     author_email='kumaki0@gmail.com',
-    install_requires=['invoke'],
+    install_requires=['invoke', 'pyyaml'],
     url='https://github.com/kumak1/kumaoche',
     license=license,
-    packages=find_packages(exclude=('tests', 'docs')),
+    packages=find_packages(exclude=('tests', 'docs', 'examples')),
     test_suite='tests'
 )
