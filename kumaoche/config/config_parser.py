71bea4a80349870940bb3147f6a1cba0e5f07b8c
kumak1 <makino@pepabo.com>
2019-11-20 16:39:54 +0900

    config の引数にファイルパスを渡せるように変更

--- kumaoche/config/config_parser.py
+++ kumaoche/config/config_parser.py
@@ -10,22 +10,25 @@ from .config_pack import ConfigPack
 class ConfigParser(object):
 
     @classmethod
-    def file_path(cls):
-        paths = os.environ.get('KUMAOCHE_CONFIG_PATH', os.getcwd() + '/kumaoche_config.yml').split(':')
+    def file_path_list(cls):
+        path_list = os.environ.get('KUMAOCHE_CONFIG_PATH', os.getcwd() + '/kumaoche_config.yml').split(':')
 
         # ファイル存在確認
-        for path in paths:
+        for path in path_list:
             if not os.path.exists(path):
                 print(f'Target config file "{path}" is not exist.')
                 sys.exit()
 
-        return paths
+        return path_list
 
     @classmethod
-    def all_roles(cls):
+    def all_roles(cls, file_path_list=[]):
+        if len(file_path_list) == 0:
+            file_path_list = cls.file_path_list()
+
         loaded_roles = []
 
-        for file_path in cls.file_path():
+        for file_path in file_path_list:
             with open(file_path) as file:
                 loaded_roles += list(filter(lambda x: x != 'defaults', yaml.safe_load(file).keys()))
 
@@ -33,13 +36,17 @@ class ConfigParser(object):
         return sorted(set(loaded_roles), key=loaded_roles.index)
 
     @classmethod
-    def find(cls, role: str):
+    def find(cls, role: str, file_path_list=[]):
+        if len(file_path_list) == 0:
+            file_path_list = cls.file_path_list()
+
         if role not in cls.all_roles():
             print(f'Target role "{role}" is not exist.')
             sys.exit()
 
         loaded_yaml = {}
-        for file_path in cls.file_path():
+
+        for file_path in file_path_list:
             with open(file_path) as file:
                 loaded_yaml = {**loaded_yaml, **yaml.safe_load(file)}
 
