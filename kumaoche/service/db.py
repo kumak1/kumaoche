ab50ffa9e63bec0ebdeeda5fa339460d53886007
kumak1 <makino@pepabo.com>
2019-11-22 14:29:38 +0900

    service のクラス変数 env を private にする

--- kumaoche/service/db.py
+++ kumaoche/service/db.py
@@ -7,15 +7,15 @@ from .service import Service
 
 class DB(Service):
     def __init__(self, env: ExecEnv, config: DbConfig):
-        self.env = env
+        self.__env = env
         self.__config = config
 
     def run(self, command: str):
-        var_command = {'command': self.env.var_assign(command)}
-        return self.env.run(self.env.var_assign(self.__config.run,  var_command))
+        var_command = {'command': self.__env.var_assign(command)}
+        return self.__env.run(self.__env.var_assign(self.__config.run, var_command))
 
     def setup(self):
-        return self.env.run(self.env.var_assign(self.__config.setup))
+        return self.__env.run(self.__env.var_assign(self.__config.setup))
 
     def update(self):
-        return self.env.run(self.env.var_assign(self.__config.update))
+        return self.__env.run(self.__env.var_assign(self.__config.update))
--- kumaoche/service/package_manager.py
+++ kumaoche/service/package_manager.py
@@ -7,23 +7,23 @@ from .service import Service
 
 class PackageManager(Service):
     def __init__(self, env: ExecEnv, config: PackageManagerConfig):
-        self.env = env
+        self.__env = env
         self.__config = config
 
     def run(self, command: str):
-        var_command = {'command': self.env.var_assign(command)}
-        return self.env.run(self.env.var_assign(self.__config.run,  var_command))
+        var_command = {'command': self.__env.var_assign(command)}
+        return self.__env.run(self.__env.var_assign(self.__config.run, var_command))
 
     def setup(self):
-        return self.env.run(self.env.var_assign(self.__config.setup))
+        return self.__env.run(self.__env.var_assign(self.__config.setup))
 
     def update(self):
-        return self.env.run(self.env.var_assign(self.__config.update))
+        return self.__env.run(self.__env.var_assign(self.__config.update))
 
     def test(self, suffix=''):
-        cmd = self.env.var_assign(self.__config.test)
+        cmd = self.__env.var_assign(self.__config.test)
 
         if suffix != '':
-            suffix = ' ' + self.env.var_assign(suffix)
+            suffix = ' ' + self.__env.var_assign(suffix)
 
-        return self.env.run(f'{cmd}{suffix}')
+        return self.__env.run(f'{cmd}{suffix}')
