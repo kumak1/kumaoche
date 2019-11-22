ebf6d1001fa4d1ea432b99435017e5f70b63ade3
kumak1 <makino@pepabo.com>
2019-11-22 15:48:42 +0900

    update config parser

--- kumaoche/config/config_parser.py
+++ kumaoche/config/config_parser.py
@@ -15,27 +15,24 @@ class ConfigParser(object):
 
     @classmethod
     def all_roles(cls, file_path_list=None):
-        if file_path_list is None:
-            file_path_list = cls.file_path_list()
-
-        loaded_roles = []
-
-        for file_path in file_path_list:
-            with open(file_path) as file:
-                loaded_roles += list(filter(lambda x: x != 'defaults', yaml.safe_load(file).keys()))
+        loaded_yaml = cls.yaml_load(file_path_list)
+        parsed_yaml = cls.parse_version(loaded_yaml)
+        loaded_roles = parsed_yaml.get('roles', {})
 
         # 順序を維持した重複削除
         return sorted(set(loaded_roles), key=loaded_roles.index)
 
     @classmethod
     def find(cls, role: str, file_path_list=None):
-        if role not in cls.all_roles(file_path_list):
+        loaded_yaml = cls.yaml_load(file_path_list)
+        parsed_yaml = cls.parse_version(loaded_yaml)
+        loaded_roles = parsed_yaml.get('roles', {})
+
+        if role not in loaded_roles.keys():
             print(f'Target role "{role}" is not exist.')
             sys.exit()
 
-        loaded_yaml = cls.yaml_load(file_path_list)
-
-        return ConfigPack(loaded_yaml.get(role, {}))
+        return ConfigPack(loaded_roles.get(role, {}))
 
     @classmethod
     def yaml_load(cls, file_path_list=None):
@@ -54,3 +51,13 @@ class ConfigParser(object):
                 loaded_yaml = {**loaded_yaml, **yaml.safe_load(file)}
 
         return loaded_yaml
+
+    @classmethod
+    def parse_version(cls, loaded_yaml):
+        version = int(loaded_yaml.get('version', 1))
+
+        if version == 1:
+            return loaded_yaml
+        else:
+            print(f'Yaml version "{version}" is invalid.')
+            sys.exit()
