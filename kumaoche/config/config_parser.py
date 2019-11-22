cbd9123e75b62c3adcdd1fc462af528df97244ee
kumak1 <makino@pepabo.com>
2019-11-22 11:26:15 +0900

    update config parser

--- kumaoche/config/config_parser.py
+++ kumaoche/config/config_parser.py
@@ -11,15 +11,7 @@ class ConfigParser(object):
 
     @classmethod
     def file_path_list(cls):
-        path_list = os.environ.get('KUMAOCHE_CONFIG_PATH', os.getcwd() + '/kumaoche_config.yml').split(':')
-
-        # ファイル存在確認
-        for path in path_list:
-            if not os.path.exists(path):
-                print(f'Target config file "{path}" is not exist.')
-                sys.exit()
-
-        return path_list
+        return os.environ.get('KUMAOCHE_CONFIG_PATH', os.getcwd() + '/kumaoche_config.yml').split(':')
 
     @classmethod
     def all_roles(cls, file_path_list=None):
@@ -37,17 +29,28 @@ class ConfigParser(object):
 
     @classmethod
     def find(cls, role: str, file_path_list=None):
-        if file_path_list is None:
-            file_path_list = cls.file_path_list()
-
         if role not in cls.all_roles(file_path_list):
             print(f'Target role "{role}" is not exist.')
             sys.exit()
 
-        loaded_yaml = {}
+        loaded_yaml = cls.yaml_load(file_path_list)
 
+        return ConfigPack(loaded_yaml.get(role, {}))
+
+    @classmethod
+    def yaml_load(cls, file_path_list=None):
+        if file_path_list is None:
+            file_path_list = cls.file_path_list()
+
+        # ファイル存在確認
+        for path in file_path_list:
+            if not os.path.exists(path):
+                print(f'Target config file "{path}" is not exist.')
+                sys.exit()
+
+        loaded_yaml = {}
         for file_path in file_path_list:
             with open(file_path) as file:
                 loaded_yaml = {**loaded_yaml, **yaml.safe_load(file)}
 
-        return ConfigPack(loaded_yaml.get(role, {}))
+        return loaded_yaml
